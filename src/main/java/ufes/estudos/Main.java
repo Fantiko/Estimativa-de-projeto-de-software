028bf960b39c2f379526214aca4f5f113382f963
lapaCaio <lapacaio2001@gmail.com>
2025-08-27 11:36:56 -0300

    UPD - correção da master

--- src/main/java/ufes/estudos/Main.java
+++ src/main/java/ufes/estudos/Main.java
@@ -2,18 +2,14 @@ package ufes.estudos;
 
 import ufes.estudos.Presenter.LoginPresenter;
 import ufes.estudos.Views.TelaLogin;
-import ufes.estudos.dao.SetupDAO; // <<< IMPORT ADICIONADO
+import ufes.estudos.dao.SetupDAO;
 
 public class Main {
     public static void main(String[] args) {
-        // --- LÓGICA DE INICIALIZAÇÃO DO BANCO ADICIONADA AQUI ---
-        // Garante que as tabelas existam e que os dados base sejam inseridos
         SetupDAO setup = new SetupDAO();
         setup.criaTabelas();
         setup.popularTabelasBase();
-        // --- FIM DA LÓGICA DE INICIALIZAÇÃO ---
 
-        // O resto do programa continua normalmente
         TelaLogin view = new TelaLogin();
         new LoginPresenter(view);
         view.setVisible(true);
